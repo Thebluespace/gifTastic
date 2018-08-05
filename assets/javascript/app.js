3d84b60e0b103946efa8b1aa904db9920a82581c
Thomas Greene <tecgreene93@gmail.com>
2018-08-05 18:52:06 -0400

    added functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-;var appObj = {
+var appObj = {
     keywords: ["cat", "Rick and Morty", "Mario", "amazing","funny","doge"],
     saved: {
 
@@ -6,6 +6,8 @@
     buttoncount: 0,
     queryURL: "https://api.giphy.com/v1/gifs/search?api_key=nXeYSnoxMTwVbTAhSpvKiYWczNv5saTx&limit=10&lang=en&q=",
     rating: "&rating=",
+    gifcount: -1,
+    lasttag: "",
 };
 // API KEY nXeYSnoxMTwVbTAhSpvKiYWczNv5saTx
 function getGiphy(event) {
@@ -17,42 +19,75 @@ function getGiphy(event) {
                 addPhrase($(".usertext").val());
                 var queryURL = appObj.queryURL + $(".usertext").val();
             }
-        } else {
+        } else if ($(event.target).attr("id") != "getGiphier"){
             var queryURL = appObj.queryURL + $(event.target).text();
         }
-        console.log($(event));
+        if ($(event.target).attr("id") != "getGiphier"){
+            $(".imagelocation").empty();
+            appObj.lasttag = $(event.target).text();
+            appObj.gifcount = 0;
+        } else if ($(event.target).attr("id") == "getGiphier") {
+            event.preventDefault();
+            if( appObj.gifcount === 0){
+                appObj.gifcount += 10;
+                var queryURL = appObj.queryURL + appObj.lasttag;
+                queryURL += "&offset=" + appObj.gifcount;
+            } else {
+                return;
+            }
+        }
         
-        queryURL += "&limit=10"
+        queryURL += "&limit=10" + appObj.rating + $(".userselect").val();
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
             try {
-            console.log(response);
-            $(".imagelocation").empty();
-            for (i=0; response.data.length -1; i++){
+            for (i=0; i < response.data.length; i++){
                 
-                    var data = response.data[i].images.fixed_height;
+                    var data = response.data[i].images;
                     var img = $("<img>");
                     img.addClass("gif");
                     img.attr("id","gif"+i);
-                    img.attr("src", data.url)
-                    img.attr("height",data.height);
-                    img.attr("height",data.height);
+                    img.attr("src", data.original_still.url);
+                    img.attr("height",data.fixed_height.height);
+                    img.attr("height",data.fixed_height.height);
+                    img.attr("data-still", data.original_still.url);
+                    img.attr("data-gif", data.fixed_height.url);
+                    img.attr("state","still");
                     var gifDiv = $("<div>");
                     gifDiv.addClass("gifdiv");
+                    var title = $("<h1>");
+                    title.addClass("title");
+                    title.text(response.data[i].title);
+                    gifDiv.append(title);
                     gifDiv.append(img);
                     var rating = $("<h2>");
                     rating.addClass("rating");
                     rating.text("Rating : " + response.data[i].rating);
                     gifDiv.append(rating);
-                    $(".imagelocation").append(gifDiv);        
+                    img.on("click", gifClick);
+                    $(".imagelocation").append(gifDiv);
+
                 }
             } catch (error) {
                 console.error(error);
                 i++;
             }
         });   
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function gifClick(event) {
+    try {
+        console.log(event.target);
+        if($(event.target).attr("state") == "still"){
+            $(event.target).attr("src",$(event.target).attr("data-gif"));
+        } else {
+            $(event.target).attr("src",$(event.target).attr("data-still"));
+        }
     } catch (error) {
         console.log(error);
     }
@@ -73,6 +108,7 @@ function addPhrase(text){
 }
 
 function load(){
+    try {
         // load buttongs
         for (i=0;i<appObj.keywords.length - 1;i++){
             appObj.buttoncount++;
@@ -84,21 +120,34 @@ function load(){
         }
 
         var userh1 = $("<label>");
-        userh1.addClass("");
+        userh1.addClass("banner");
         userh1.text("Enter a Keyphrase to search!");
         var usertext = $("<input>");
         usertext.attr("type","text");
         usertext.addClass("usertext");
+        var userh2 = $("<label>");
+        userh2.addClass("banner");
+        userh2.text("Select a Rating Cap:");
+        var usertext2 = $("<select>");
+        usertext2.html("<option>G</option><option>PG</option><option>PG-13</option><option>R</option>")
+        usertext2.addClass("userselect");
         var button = $("<button>");
         button.addClass("button");
         button.attr("id","addPhrase");
         button.text("Get Giphy");
+        var button2 = $("<button>");
+        button2.addClass("button");
+        button2.attr("id","getGiphier");
+        button2.text("Get Giphier");
 
-        $(".userinput").append(userh1,$("<br>"),usertext,$("<br>"),button);
+        $(".userinput").append(userh2,$("<br>"),usertext2,$("<br>"),$("<br>"),userh1,$("<br>"),usertext,$("<br>"),button,button2);
         // $(".userinput").append(usertext);
         // $(".userinput").append(button);
 
         $(document.body).on("click",".button", getGiphy)
-
+    
+    } catch (error) {
+        console.log(error);
+    }
 
 }
